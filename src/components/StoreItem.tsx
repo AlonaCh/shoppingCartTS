59ccf5d2b0d56cd41c6120ef56317b29e2350aeb
Alona Chubenko <aliona.chubenko@gmail.com>
2024-06-09 13:06:08 +0300

    Types for context provider

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -36,7 +36,7 @@ export function StoreItem({id, name, price, imgUrl}: StoreItemsProps) {
 <Button >+</Button>
 
                 </div>
-                <Button variant='danger>Remove</Button>
+                <Button variant='danger' size="sm">Remove</Button>
                 </div>}
                 </div>
             </Card.Body>
--- src/context/shoppingCartContext.tsx
+++ src/context/shoppingCartContext.tsx
@@ -1,11 +1,26 @@
 import { createContext, useContext, ReactNode } from "react";
+import { useState } from "react";
 
 //type of choldren property inside of react
 type ShoppingCartProviderProps = {
     children: ReactNode
 }
 
-const ShoppingCartContext = createContext({});
+//we will have 4 functions in our context
+type ShoppingCartContext = {
+    getItemQuantity: (id: number) => number;
+    increaseCartQuantity: (id: number) => void;
+    decreaseCartQuantity: (id: number) => void;
+    removeItem: (id: number) => void;
+
+}
+
+type CartItem ={
+    id: number;
+    quantity: number;
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
 export function useShoppingCart(){
     return useContext(ShoppingCartContext)
@@ -14,6 +29,8 @@ export function useShoppingCart(){
 
 //wrapper around our context that has children object. We are gonna re-render those children. 
 export function ShoppingCartProvider({children}: ShoppingCartProviderProps){
+    const [cartItem, setCartItem]=useState<CartItem[]>([])
+
 return <ShoppingCartContext.Provider value={{}}>{children}
 </ShoppingCartContext.Provider> //rerender those children
 }
